0422d6c3e170d75f16c1b92402e448a0b23d579f
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2017-12-29 17:54:47 +0100

    Setup basic functionality

---
+++ src/components/Light.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { HueStore } from '../stores/HueStore'
+import HueDispatcher from '../dispatchers/HueDispatcher'
+import styled from 'styled-components'
+
+export default class Light extends Component {
+	constructor (props) {
+		super(props)
+		this.props = props
+	}
+
+	componentDidUmnount () {
+		HueStore.addListener('change', this.onChange)
+	}
+
+	componentWillUnmount () {
+		HueStore.removeListener('change', this.onChange)
+	}
+
+	onChange = () => {
+		this.setState(HueStore.getState())
+	}
+
+	turnOn = () => {
+		HueDispatcher.dispatch({
+			actionName: 'LIGHT_ON',
+			data: this.props.lightId
+		})
+	}
+
+	turnOff = () => {
+		HueDispatcher.dispatch({
+			actionName: 'LIGHT_OFF',
+			data: this.props.lightId
+		})
+	}
+
+	render () {
+		return (
+			<Wrapper>
+				<p>Light {this.props.lightId}</p>
+				<input type="button" onClick={this.turnOn} value="On" />
+				<input type="button" onClick={this.turnOff} value="Off" />
+			</Wrapper>
+		)
+	}
+}
+
+const Wrapper = styled.div`
+	width: 200px;
+	height: 200px;
+	background-color: #ff0069;
+`
